223f39971177fa8fc9621e548af8210b3dc88bed
AutisticRobot <tmkormann04@gmail.com>
2023-08-05 23:18:49 -0400

    made a hello world

---
+++ main.cpp
@@ -0,0 +1,12 @@
+// main.cpp
+// just a hello world program right now
+// following a tutorial by Mary Elaine Califf
+// made by Tyler M. Kormann
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    cout << "Hello, world!\n";
+}
