64eb3bd084b7caae21e122c0d33558f9ad5edb5d
AutisticRobot <tmkormann04@gmail.com>
2023-08-07 10:37:11 -0400

    got functions down

--- main.cpp
+++ main.cpp
@@ -5,12 +5,26 @@
 
 #include <iostream>
 using namespace std;
+int testFunc(int num1, int num2);
+
 
 int main(int argc, char *argv[])
 {
-    int age;
+    int in1;
+    int in2;
 
-    cout << "What is your age? ";
-    cin >> age;
-    cout << "Your age is " << age << endl;
+    cout << "What is your first number? ";
+    cin >> in1;
+    cout << "What is your second number? ";
+    cin >> in2;
+    cout << "Your total is " << testFunc(in1, in2) << endl;
+}
+
+int testFunc(int num1, int num2)
+{
+    int out;
+
+    out = num1 + num2;
+
+    return out;
 }
