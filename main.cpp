a40f6979cced76ac225979cc589329bc973449ae
AutisticRobot <tmkormann04@gmail.com>
2023-08-08 21:20:55 -0400

    started on the actiuial program

--- main.cpp
+++ main.cpp
@@ -1,30 +1,22 @@
 // main.cpp
-// just a tutorial program dump right now
-// following a tutorial by Mary Elaine Califf
-// made by Tyler M. Kormann
+// main file for a schiule making and testing program.
+// made by Tyler M. Kormann (tylerkormann.com)
 
 #include <iostream>
 using namespace std;
-int testFunc(int num1, int num2);
 
+int tDays;
+int onDays;
+int offDays;
 
 int main(int argc, char *argv[])
 {
-    int in1;
-    int in2;
+    cout << "days in a work week: ";
+    cin >> tDays;
+    cout << "working days in a work week: ";
+    cin >> onDays;
+    cout << "non-working days in a work week: ";
+    cin >> offDays;
 
-    cout << "What is your first number? ";
-    cin >> in1;
-    cout << "What is your second number? ";
-    cin >> in2;
-    cout << "Your total is " << testFunc(in1, in2) << endl;
-}
-
-int testFunc(int num1, int num2)
-{
-    int out;
-
-    out = num1 + num2;
-
-    return out;
+    
 }
