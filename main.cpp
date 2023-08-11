041e658dda4710bdcd445685a32944bb68e3c317
AutisticRobot <80130251+AutisticRobot@users.noreply.github.com>
2023-08-10 21:41:35 -0700

    added getDay function

--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 // main.cpp
-// main file for a schiule making and testing program.
+// main file for a schedule making and testing program.
 // made by Tyler M. Kormann (tylerkormann.com)
 
 #include <iostream>
@@ -9,7 +9,11 @@ int tDays;
 int onDays;
 int offDays;
 
-void display();
+void displayMonth(int offset = 0, int displayDays = 0);// display the curent schedule for a month;
+void displayWeek(int offset = 0);//helper function to display a week;
+
+bool getDay(int day);//returns the type of the input day;
+//(true=onDay,false=offDay);
 
 int main(int argc, char *argv[])
 {
@@ -20,20 +24,32 @@ int main(int argc, char *argv[])
     cout << "non-working days in a work week: ";
     cin >> offDays;
 
-    
+    displayMonth(1,30);
 }
 
-void display()
+void displayMonth(int offset, int displayDays)// display the curent schedule for a month;
 {
-    cout << "#---#---#---#---#---#---#---#";
-    cout << "|Sun|Mon|Tue|Wed|Thr|Fri|Sat|";
-    cout << "#---#---#---#---#---#---#---#";
-    cout << "|   |   |   |   |   |   |   |";
-    cout << "#---#---#---#---#---#---#---#";
-    cout << "|   |   |   |   |   |   |   |";
-    cout << "#---#---#---#---#---#---#---#";
-    cout << "|   |   |   |   |   |   |   |";
-    cout << "#---#---#---#---#---#---#---#";
-    cout << "|   |   |   |   |   |   |   |";
-    cout << "#---#---#---#---#---#---#---#";
+    cout << "#---#---#---#---#---#---#---#" << endl;
+    cout << "|Sun|Mon|Tue|Wed|Thr|Fri|Sat|" << endl;
+    cout << "#---#---#---#---#---#---#---#" << endl;
+    displayWeek();
+    displayWeek();
+    displayWeek();
+}
+
+void displayWeek(int offset)//helper function to display a week;
+{
+    cout << "|   |   |   |   |   |   |   |" << endl;
+    cout << "#---#---#---#---#---#---#---#" << endl;
+}
+
+bool getDay(int day)//returns the type of the input day;
+{
+    int dayOfWeek = day % tDays;
+    if(dayOfWeek > onDays)
+    {
+        return false;
+    }else{
+        return true;
+    }
 }
