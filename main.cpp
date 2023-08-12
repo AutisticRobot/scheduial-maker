b04fc923e02974eaea0de5981bf2997a27bd56ac
AutisticRobot <tmkormann04@gmail.com>
2023-08-11 19:11:10 -0700

    completed individuial schedule making

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int tDays;
 int onDays;
 int offDays;
 
-void displayMonth(int offset = 0, int displayDays = 0);// display the curent schedule for a month;
+void displayMonth(int offset = 0, int displayDays = 30);// display the curent schedule for a month;
 void displayWeek(int offset = 0);//helper function to display a week;
 
 bool getDay(int day);//returns the type of the input day;
@@ -17,14 +17,30 @@ bool getDay(int day);//returns the type of the input day;
 
 int main(int argc, char *argv[])
 {
-    cout << "days in a work week: ";
-    cin >> tDays;
-    cout << "working days in a work week: ";
-    cin >> onDays;
-    cout << "non-working days in a work week: ";
-    cin >> offDays;
+    bool validLayout = false;
+    while(!validLayout)
+    {
+        cout << "days in a work week: ";
+        cin >> tDays;
+        cout << "working days in a work week: ";
+        cin >> onDays;
+        cout << "non-working days in a work week: ";
+        cin >> offDays;
 
-    displayMonth(1,30);
+        if((onDays + offDays) != tDays)
+        {
+            if((onDays + offDays) > tDays)
+            {
+                cout << "Too many days" << endl;
+            }else{
+                cout << "Not enough days" << endl;
+            }
+        }else{
+            validLayout = true;
+        }
+    }
+
+    displayMonth(-1,30);
 }
 
 void displayMonth(int offset, int displayDays)// display the curent schedule for a month;
@@ -32,21 +48,40 @@ void displayMonth(int offset, int displayDays)// display the curent schedule for
     cout << "#---#---#---#---#---#---#---#" << endl;
     cout << "|Sun|Mon|Tue|Wed|Thr|Fri|Sat|" << endl;
     cout << "#---#---#---#---#---#---#---#" << endl;
-    displayWeek();
-    displayWeek();
-    displayWeek();
+    displayWeek(0+offset);
+    displayWeek(7+offset);
+    displayWeek(14+offset);
 }
 
 void displayWeek(int offset)//helper function to display a week;
 {
-    cout << "|   |   |   |   |   |   |   |" << endl;
+    for(int i=0;i<7;i++)
+    {
+        bool actDay = getDay(offset++);
+        cout << "| ";
+        if(actDay)
+        {
+            cout << "X ";
+        }else{
+            cout << "  ";
+        }
+    }
+    cout << "|" << endl;
     cout << "#---#---#---#---#---#---#---#" << endl;
 }
 
 bool getDay(int day)//returns the type of the input day;
 {
-    int dayOfWeek = day % tDays;
-    if(dayOfWeek > onDays)
+    int dayOfWeek;
+    if(day > 0)
+    {
+        dayOfWeek = day % tDays;
+    }else{
+        dayOfWeek = -day % tDays;
+        dayOfWeek = tDays - dayOfWeek;
+        dayOfWeek = dayOfWeek % tDays;
+    }
+    if(dayOfWeek >= onDays)
     {
         return false;
     }else{
